0443c3ebf923b3bba2215fa126e6b96dade7efc3
Ryan Little <ryan.jay.little@gmail.com>
2020-11-01 14:30:56 -0800

    Added crypto utils file

---
+++ crypto_utils.py
@@ -0,0 +1,27 @@
+from cryptography.hazmat.primitives import hashes, hmac
+
+def p_hash(secret: bytes, seed: bytes, num_bytes: int) -> bytes:
+    hmac_ = hmac.HMAC(secret, hashes.SHA256())
+    A = seed
+    output = b''
+    while len(output) < num_bytes:
+        hmac_A = hmac_.copy()
+        hmac_P = hmac_.copy()
+        hmac_A.update(A)
+        A = hmac_A.finalize()
+        hmac_P.update(A + seed)
+        output += hmac_P.finalize()
+    return output[:num_bytes]
+
+
+def PRF(secret: bytes, label: bytes, seed: bytes, num_bytes: int) -> bytes:
+    return p_hash(secret, label + seed, num_bytes)
+
+
+# This exists because I want to hash the records of the handshake in
+# tls_session, but I don't want to bring in any crypto imports in the main
+# files. Might want to do some refactoring 
+def sha256(data: bytes) -> bytes:
+    hash_ = hashes.Hash(hashes.SHA256())
+    hash_.update(data)
+    return hash_.finalize()
