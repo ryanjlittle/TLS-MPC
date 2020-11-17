135ea7a577fbaed1c3f2601391110443834ffbb5
Ryan Little <ryan.jay.little@gmail.com>
2020-11-16 20:56:47 -0800

    Explicit nonce was named implicit_nonce, oops

--- ciphers.py
+++ ciphers.py
@@ -20,14 +20,14 @@ class Cipher():
 
 class AES_GCM(Cipher):
     
-    def __init__(self, key: bytes, salt: bytes):
+    def __init__(self, key: bytes, implicit_nonce: bytes):
         self.aes = AESGCM(key)
-        self.salt = salt
+        self.implicit_nonce = implicit_nonce
         self.sequence_num = 0
 
 
     def encrypt(self, data: bytes, additional_data: bytes):
-        ciphertext = self.aes.encrypt(self._getNonce(), data, additional_data)
+        ciphertext = self.aes.encrypt(self._encryptionNonce(), data, additional_data)
         self.sequence_num += 1
         return ciphertext
 
@@ -36,14 +36,14 @@ class AES_GCM(Cipher):
     def createHandshakeFinishedPacket(self, verify_data: bytes) -> ClientHandshakeFinished:
         additional_data = self.sequence_num.to_bytes(8, byteorder='big') \
                           + b'\x16\x03\x03\x00\x10' 
-        implicit_nonce = self._getImplicitNonce()
+        explicit_nonce = self._getExplicitNonce()
         # Prepend a handshake header to the data
         payload = b'\x14\x00\x00\x0c' + verify_data
         ciphertext = self.encrypt(payload, additional_data)
-        return ClientHandshakeFinished(implicit_nonce, ciphertext)
+        return ClientHandshakeFinished(explicit_nonce, ciphertext)
 
-    def _getImplicitNonce(self):
+    def _getExplicitNonce(self):
         return self.sequence_num.to_bytes(8, byteorder='big')
 
-    def _getNonce(self):
-        return self.salt + self._getImplicitNonce()
+    def _encryptionNonce(self):
+        return self.implicit_nonce + self._getExplicitNonce()
