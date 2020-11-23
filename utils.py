17d3d8748484eb1c61d1e7da603c764461c702ce
Ryan Little <ryan.jay.little@gmail.com>
2020-11-22 16:04:07 -0800

    Cleaned up hexdump a bit

--- utils.py
+++ utils.py
@@ -12,11 +12,12 @@ def prependedLen(data: bytes, numBytes=2):
     return lengthBytes + data
 
 def hexdump(data: bytes) -> str:
-    groups = [data[i:i+16] for i in range(0,len(data), 16)]
+    printable = string.ascii_letters + string.digits + string.punctuation + ' '
+    groups = [data[i:i+16] for i in range(0, len(data), 16)]
     lines = []
     for group in groups:
         line = binascii.hexlify(group, ' ').decode('ascii').ljust(51, ' ')
         # Also print the ascii representation
-        line += ''.join([chr(b) if chr(b) in string.digits+string.ascii_letters+' ' else '.' for b in group])
+        line += ''.join([chr(b) if chr(b) in printable else '.' for b in group])
         lines += [line]
     return '\n'.join(lines)
